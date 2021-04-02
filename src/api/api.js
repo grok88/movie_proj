371adb241f6fde4d2604dfb526df9a28ff037ef4
Alex <grok88@tut.by>
2021-04-02 14:16:50 +0300

    initial commit

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://api.themoviedb.org/3";
+
+export const API_KEY_3 = "74cf872c374464de94ecd087d053d256";
+
+export const API_KEY_4 =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NGNmODcyYzM3NDQ2NGRlOTRlY2QwODdkMDUzZDI1NiIsInN1YiI6IjYwNjVkMGVjZDEzMzI0MDAyOTU3YWY4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xDMzqEfiTRACW-aKuOO0uQJnWFWRNvmG4B6a2rsmxyE";
