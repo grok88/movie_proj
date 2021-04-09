0d84907e261089a3b08a8aca0de5ffbf8a20012c
Alex <grok88@tut.by>
2021-04-09 10:58:08 +0300

    refactor Promises chain :  add getRequestToken()

--- src/components/Header/Login/Login.tsx
+++ src/components/Header/Login/Login.tsx
@@ -2,64 +2,98 @@ import axios from 'axios';
 import React, {PureComponent} from 'react';
 import {API_KEY_3, API_URL, GetToken} from '../../../api/api';
 
+
 class Login extends PureComponent {
     onSend = () => {
         let tokenUrl = `${API_URL}/authentication/token/new?api_key=${API_KEY_3}`;
-        //get token
-        axios.get<GetToken>(tokenUrl).then(res => res.data)
-            .then(res => {
-                //login url
-                let loginUrl = `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`;
-                // axios.post<GetToken>(loginUrl, {
-                //         headers: {
-                //             'Content-Type': 'application/json'
-                //         },
-                //         mode: 'cors',
-                //     },{
-                //         username: 'Alex_Gor',
-                //         password: 'thisIsSparta',
-                //         request_token: res.request_token
-                //     }
-                // ).then(res => res.data)
-                //     .then(res => {
-                //         debugger
-                //     })
-                fetch(loginUrl,
-                    {
-                        method: 'Post',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        mode: 'cors',
-                        body: JSON.stringify({
-                            username: 'Alex_Gor',
-                            password: 'thisissparta',
-                            request_token: res.request_token
-                        })
-                    },
-                )
-                    .then(res => res.json())
-                    .then(res => {
-                        //session url
-                        let loginUrl = `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`;
-                        fetch(loginUrl,
-                            {
-                                method: 'Post',
-                                headers: {
-                                    'Content-Type': 'application/json'
-                                },
-                                mode: 'cors',
-                                body: JSON.stringify({
-                                    request_token: res.request_token
-                                })
-                            },
-                        )
-                            .then(res => res.json())
-                            .then(res => {
-                                console.log(res)
-                            })
+
+        const getRequestToken = () => {
+            return new Promise((res, rej) => {
+                axios.get<GetToken>(tokenUrl).then(res => {
+                    if (res.status < 400) {
+                        return res.data
+                    } else {
+                        throw res
+                    }
+                }).catch((err) => {
+                    // console.dir(err.response.data.status_message)
+                    rej(err.response.data.status_message);
+                })
+                    .then(data => {
+                        res(data);
                     })
+                // .catch(error => {
+                //     console.log(error)
+                //     rej(error)
+                // })
             })
+
+        }
+        getRequestToken()
+            .then(res => {
+                console.log('success', res);
+            })
+            .catch(error => {
+                console.log('error : ', error)
+            })
+        // //get token
+        // axios.get<GetToken>(tokenUrl).then(res => {
+        //     console.log(res)
+        //     return res.data
+        // })
+        //     .then(res => {
+        //         //login url
+        //         let loginUrl = `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`;
+        //         // axios.post<GetToken>(loginUrl, {
+        //         //         headers: {
+        //         //             'Content-Type': 'application/json'
+        //         //         },
+        //         //         mode: 'cors',
+        //         //     },{
+        //         //         username: 'Alex_Gor',
+        //         //         password: 'thisIsSparta',
+        //         //         request_token: res.request_token
+        //         //     }
+        //         // ).then(res => res.data)
+        //         //     .then(res => {
+        //         //         debugger
+        //         //     })
+        //         fetch(loginUrl,
+        //             {
+        //                 method: 'Post',
+        //                 headers: {
+        //                     'Content-Type': 'application/json'
+        //                 },
+        //                 mode: 'cors',
+        //                 body: JSON.stringify({
+        //                     username: 'Alex_Gor',
+        //                     password: 'thisissparta',
+        //                     request_token: res.request_token
+        //                 })
+        //             },
+        //         )
+        //             .then(res => res.json())
+        //             .then(res => {
+        //                 //session url
+        //                 let loginUrl = `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`;
+        //                 fetch(loginUrl,
+        //                     {
+        //                         method: 'Post',
+        //                         headers: {
+        //                             'Content-Type': 'application/json'
+        //                         },
+        //                         mode: 'cors',
+        //                         body: JSON.stringify({
+        //                             request_token: res.request_token
+        //                         })
+        //                     },
+        //                 )
+        //                     .then(res => res.json())
+        //                     .then(res => {
+        //                         console.log(res)
+        //                     })
+        //             })
+        //     })
     }
 
     render() {
