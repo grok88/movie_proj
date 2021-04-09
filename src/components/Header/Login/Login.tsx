1bb6cdb93dc425fcc0d162b92c1d08c9ed349239
Alex <grok88@tut.by>
2021-04-09 15:56:14 +0300

    Promises chain :  added 3 request

--- src/components/Header/Login/Login.tsx
+++ src/components/Header/Login/Login.tsx
@@ -2,98 +2,120 @@ import axios from 'axios';
 import React, {PureComponent} from 'react';
 import {API_KEY_3, API_URL, GetToken} from '../../../api/api';
 
+type ResponseWithLoginType = {
+    success: boolean,
+    expires_at: string,
+    request_token: string
+}
 
 class Login extends PureComponent {
     onSend = () => {
         let tokenUrl = `${API_URL}/authentication/token/new?api_key=${API_KEY_3}`;
 
         const getRequestToken = () => {
-            return new Promise((res, rej) => {
-                axios.get<GetToken>(tokenUrl).then(res => {
-                    if (res.status < 400) {
-                        return res.data
-                    } else {
-                        throw res
-                    }
-                }).catch((err) => {
-                    // console.dir(err.response.data.status_message)
-                    rej(err.response.data.status_message);
+            return axios.get<GetToken>(tokenUrl).then(res => res.data)
+                .catch((err) => {
+                    return err.response.data.status_message;
                 })
-                    .then(data => {
-                        res(data);
-                    })
-                // .catch(error => {
-                //     console.log(error)
-                //     rej(error)
-                // })
-            })
-
         }
+        const fetchApi = (url: string, options: any) => {
+            return new Promise((res, rej) => {
+                fetch(url, options)
+                    .then(res => {
+                            if (res.status < 400) {
+                                return res.json();
+                            } else {
+                                throw res
+                            }
+                        }
+                    ).then(data => {
+                    res(data);
+                })
+                    .catch(response => {
+                        response.json()
+                            .then((err: any) => {
+                                rej(err)
+                            })
+                    })
+            })
+        }
+        // const validateWithToken = (body: ValidateWithTokenBodyType) => {
+        //     let loginUrl = `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`;
+        //
+        //     return new Promise((res, rej) => {
+        //         fetch(loginUrl, {
+        //             method: 'Post',
+        //             headers: {
+        //                 'Content-Type': 'application/json'
+        //             },
+        //             mode: 'cors',
+        //             body: JSON.stringify(body)
+        //         })
+        //             .then(res => {
+        //                     if (res.status < 400) {
+        //                         return res.json();
+        //                     } else {
+        //                         throw res
+        //                     }
+        //                 }
+        //             ).then(data => {
+        //             res(data);
+        //         })
+        //             .catch(response => {
+        //                 response.json()
+        //                     .then((err: any) => {
+        //                         rej(err)
+        //                     })
+        //             })
+        //     })
+        // }
+
+        // Цепочка из 3 запросов на сервер
+        // 1 за токеном
+        // 2 логин + пасс + этот токен
+        // 3 на session id
         getRequestToken()
             .then(res => {
-                console.log('success', res);
+                let loginUrl = `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`;
+                return fetchApi(loginUrl,
+                    {
+                        method: 'Post',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        mode: 'cors',
+                        body: JSON.stringify({
+                            username: 'Alex_Gor',
+                            password: 'thisissparta',
+                            request_token: res.request_token
+                        })
+                    }
+                )
+            })
+            .then((data: any) => {
+                //session url
+                let sessionUrl = `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`;
+
+                return fetchApi(sessionUrl,
+                    {
+                        method: 'Post',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        mode: 'cors',
+                        body: JSON.stringify({
+                            request_token: data.request_token
+                        })
+                    }
+                )
+                console.log(data)
+            })
+            .then(data => {
+                console.log('success', data);
             })
             .catch(error => {
                 console.log('error : ', error)
             })
-        // //get token
-        // axios.get<GetToken>(tokenUrl).then(res => {
-        //     console.log(res)
-        //     return res.data
-        // })
-        //     .then(res => {
-        //         //login url
-        //         let loginUrl = `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`;
-        //         // axios.post<GetToken>(loginUrl, {
-        //         //         headers: {
-        //         //             'Content-Type': 'application/json'
-        //         //         },
-        //         //         mode: 'cors',
-        //         //     },{
-        //         //         username: 'Alex_Gor',
-        //         //         password: 'thisIsSparta',
-        //         //         request_token: res.request_token
-        //         //     }
-        //         // ).then(res => res.data)
-        //         //     .then(res => {
-        //         //         debugger
-        //         //     })
-        //         fetch(loginUrl,
-        //             {
-        //                 method: 'Post',
-        //                 headers: {
-        //                     'Content-Type': 'application/json'
-        //                 },
-        //                 mode: 'cors',
-        //                 body: JSON.stringify({
-        //                     username: 'Alex_Gor',
-        //                     password: 'thisissparta',
-        //                     request_token: res.request_token
-        //                 })
-        //             },
-        //         )
-        //             .then(res => res.json())
-        //             .then(res => {
-        //                 //session url
-        //                 let loginUrl = `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`;
-        //                 fetch(loginUrl,
-        //                     {
-        //                         method: 'Post',
-        //                         headers: {
-        //                             'Content-Type': 'application/json'
-        //                         },
-        //                         mode: 'cors',
-        //                         body: JSON.stringify({
-        //                             request_token: res.request_token
-        //                         })
-        //                     },
-        //                 )
-        //                     .then(res => res.json())
-        //                     .then(res => {
-        //                         console.log(res)
-        //                     })
-        //             })
-        //     })
     }
 
     render() {
