0f35723e630b4d79dc425a2bed6810076ea0ce35
Alex <grok88@tut.by>
2021-05-02 19:12:04 +0300

    add gh-pages

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./stylesheets/index.css";
 import {Provider} from "react-redux";
 import {store} from "./Store/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
     , document.getElementById("root"));
