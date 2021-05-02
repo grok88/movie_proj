ec81b89780d1cbc07cfe8562ec3b556013622adb
Alex <grok88@tut.by>
2021-05-02 19:13:12 +0300

    Updates

---
+++ precache-manifest.011fe53999a22e0714dde97c7d1361f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e8d4f1fde13e2471c0309768ed2322",
+    "url": "/movie_proj/index.html"
+  },
+  {
+    "revision": "cb3fddba25950d3d4535",
+    "url": "/movie_proj/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b322c36e43bba52da55e",
+    "url": "/movie_proj/static/css/main.6fd5d7aa.chunk.css"
+  },
+  {
+    "revision": "cb3fddba25950d3d4535",
+    "url": "/movie_proj/static/js/2.b5b9716b.chunk.js"
+  },
+  {
+    "revision": "b322c36e43bba52da55e",
+    "url": "/movie_proj/static/js/main.08bccee6.chunk.js"
+  },
+  {
+    "revision": "424d50594f1395e2aaab",
+    "url": "/movie_proj/static/js/runtime~main.abe51cb4.js"
+  }
+]);
